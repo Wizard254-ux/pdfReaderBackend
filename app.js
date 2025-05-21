d026a614305775381b29e6d13edf8045386c8ad6
Wizard254-ux <brianndesa262@gmail.com>
2025-05-21 12:21:25 +0300

    hh

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const fileRoutes = require('./routes/fileRoutes');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
